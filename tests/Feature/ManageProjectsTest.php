e604706e622a89809f4eb3e2c5097326303e144b
Krystal <kaism@pm.me>
2019-06-07 13:03:14 +0200

    Update project features
    Add create

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -14,8 +14,9 @@ class ProjectsTest extends TestCase
     public function guests_cannot_manage_projects()
     {
         $project = factory('App\Project')->create();
-        
+
         $this->get('/projects')->assertRedirect('login');
+        $this->get('/projects/create')->assertRedirect('login');
         $this->get($project->path())->assertRedirect('login');
         $this->post('/projects', $project->toArray())->assertRedirect('login');
     }
@@ -25,6 +26,8 @@ class ProjectsTest extends TestCase
     {
         $this->withoutExceptionHandling();
         $this->actingAs(factory('App\User')->create());
+
+        $this->get('/projects/create')->assertStatus(200);
 
         $attributes = [
             'title' => $this->faker->sentence,
