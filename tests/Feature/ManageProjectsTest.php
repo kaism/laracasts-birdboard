14d04552da32f26d5d8f18f2fb322fd4640459ce
Krystal <kaism@pm.me>
2019-06-08 17:01:00 +0200

    Add tests helper method

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -25,7 +25,7 @@ class ProjectsTest extends TestCase
     public function a_user_can_create_a_project()
     {
         $this->withoutExceptionHandling();
-        $this->actingAs(factory('App\User')->create());
+        $this->signIn();
 
         $this->get('/projects/create')->assertStatus(200);
 
@@ -42,7 +42,7 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_user_can_view_their_project()
     {
-        $this->be(factory('App\User')->create());
+        $this->signIn();
         $this->withoutExceptionHandling();
         $project = factory('App\Project')->create(['owner_id' =>auth()->id()]);
         $this->get($project->path())
@@ -53,7 +53,7 @@ class ProjectsTest extends TestCase
     /** @test */
     public function an_authenticated_user_cannot_view_the_projects_of_others()
     {
-        $this->be(factory('App\User')->create());
+        $this->signIn();
         $project = factory('App\Project')->create();
         $this->get($project->path())->assertStatus(403);
 
@@ -62,7 +62,7 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_title()
     {
-        $this->actingAs(factory('App\User')->create());
+        $this->signIn();
         $attributes = factory('App\Project')->raw(['title' => '']);
         $this->post('/projects', $attributes)->assertSessionHasErrors('title');
     }
@@ -70,7 +70,7 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_description()
     {
-        $this->actingAs(factory('App\User')->create());
+        $this->signIn();
         $attributes = factory('App\Project')->raw(['description' => '']);
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
