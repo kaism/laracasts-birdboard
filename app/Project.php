19ca23bf65e7a29cc6be533f805302ee627cb2ec
Krystal <kaism@pm.me>
2019-06-08 17:50:33 +0200

    Add project-tasks relationship

--- app/Project.php
+++ app/Project.php
@@ -17,4 +17,10 @@ class Project extends Model
     {
     	return $this->belongsTo(User::class);
     }
+
+    public function tasks()
+    {
+    	return $this->hasMany(Task::class);
+    }
+
 }
---
+++ tests/Feature/ProjectTasksTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Project;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProjectTasksTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test */
+    public function a_project_can_have_tasks()
+    {
+        $this->withoutExceptionHandling();
+        $this->signIn();
+
+        $project = factory(Project::class)->create(['owner_id' => auth()->id()]);
+
+        $this->post($project->path() . '/tasks', ['body' => 'Test task']);
+
+        $this->get($project->path())
+            ->assertSee('Test task');
+    }
+}
