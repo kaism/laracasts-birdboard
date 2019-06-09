dc3b93e543c52fabf68231f0c0bb1c6ee96b78ed
Krystal <kaism@pm.me>
2019-06-09 21:52:50 +0200

    Fix project card spacing

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,11 +5,13 @@
 		<a href="/projects/create">New Project</a>
 	</div>
 
-	<div class="flex">
+	<div class="flex flex-wrap -mx-3">
 		@forelse ($projects as $project)
-			<div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px;">
-				<h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
-				<div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+			<div class="w-1/3 px-3 pb-6">
+				<div class="bg-white p-5 rounded shadow" style="height: 200px;">
+					<h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+					<div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+				</div>
 			</div>
 		@empty
 			<div>No projects yet.</div>
