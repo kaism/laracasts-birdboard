525b2cacf6499feda2111d3ae3ff0d3ba1fca38d
Krystal <kaism@pm.me>
2019-06-10 14:59:26 +0200

    Update project page

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,7 +3,9 @@
 @section('content')
 	<header class="flex items-center mb-3 py-4">
 		<div class="flex justify-between items-end w-full">
-			<h2 class="text-grey text-sm font-normal">My Projects</h2>
+			<p class="text-grey text-sm font-normal">
+				<a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
+			</p>
 			<a href="/projects/create" class="button">New Project</a>
 		</div>
 	</header>
@@ -11,7 +13,7 @@
 	<main>
 		<div class="lg:flex -mx-3">
 			<div class="lg:w-3/4 px-3 mb-6">
-				<div class="mb-6">
+				<div class="mb-8">
 					<h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
 					{{-- tasks --}}
 					@foreach ($project->tasks as $task)
@@ -22,7 +24,7 @@
 				<div>
 					<h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
 					{{-- general notes --}}
-					<div class="card">Lorem ipsum.</div>
+					<textarea class="card w-full" style="min-height: 200px">Lorem ipsum.</textarea>
 				</div>
 			</div>
 
