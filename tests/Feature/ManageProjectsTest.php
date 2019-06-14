0d644be3fae00c54a01eacc4f54a0594cbe5bf64
Krystal <kaism@pm.me>
2019-06-14 17:33:04 +0200

    Update project validation

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -80,7 +80,14 @@ class ManageProjectsTest extends TestCase
         $this->signIn();
         $project = factory('App\Project')->create();
         $this->get($project->path())->assertStatus(403);
+    }
 
+    /** @test */
+    public function an_authenticated_user_cannot_update_the_projects_of_others()
+    {
+        $this->signIn();
+        $project = factory('App\Project')->create();
+        $this->patch($project->path(), [])->assertStatus(403);
     }
 
     /** @test */
