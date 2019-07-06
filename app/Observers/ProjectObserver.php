d82fee3a3cef4d697c1ae1d75899c3282b336cf6
Krystal <kaism@pm.me>
2019-07-06 14:53:17 +0200

    Use model observer for projects

---
+++ app/Observers/ProjectObserver.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Observers;
+
+use App\Project;
+use App\Activity;
+
+class ProjectObserver
+{
+    /**
+     * Handle the project "created" event.
+     *
+     * @param  Project  $project
+     * @return void
+     */
+    public function created(Project $project)
+    {
+        Activity::create([
+            'project_id' => $project->id,
+            'description' => 'created'
+        ]);
+    }
+
+    /**
+     * Handle the project "updated" event.
+     *
+     * @param  Project  $project
+     * @return void
+     */
+    public function updated(Project $project)
+    {
+        Activity::create([
+            'project_id' => $project->id,
+            'description' => 'updated'
+        ]);
+    }
+
+    /**
+     * Handle the project "deleted" event.
+     *
+     * @param  Project  $project
+     * @return void
+     */
+    public function deleted(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the project "restored" event.
+     *
+     * @param  Project  $project
+     * @return void
+     */
+    public function restored(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the project "force deleted" event.
+     *
+     * @param  Project  $project
+     * @return void
+     */
+    public function forceDeleted(Project $project)
+    {
+        //
+    }
+}
