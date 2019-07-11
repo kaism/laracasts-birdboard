ab1d5a5f0ff369c104dfa653f58ea4bb558e0e40
Krystal <kaism@pm.me>
2019-07-11 17:04:18 +0200

    Record project activity on task created

--- tests/Feature/ActivityFeedTest.php
+++ tests/Feature/ActivityFeedTest.php
@@ -30,4 +30,13 @@ class ActivityFeedTest extends TestCase
         $this->assertCount(2, $project->activity);
         $this->assertEquals('updated', $project->activity->last()->description);
     }
+
+    /** @test */
+    function creating_a_new_task_records_project_activity()
+    {
+        $project = ProjectFactory::create();
+        $project->addTask('Some task');
+        $this->assertCount(2, $project->activity);
+        $this->assertEquals('created_task', $project->activity->last()->description);
+    }
 }
